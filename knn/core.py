c98b1f05f554193e657563eb854cae77faf3f468
Vitor Veras <vitorverasm@gmail.com>
2020-02-28 14:02:45 -0300

    knn first version

--- knn/core.py
+++ knn/core.py
@@ -1,7 +1,37 @@
+import numpy as np
+from scipy import stats
+
+
 class KNearestNeighborsCore:
-    def __init__(self):
-        pass
+    def __init__(self, x_train, y_train, k=3):
+        self.x_train = x_train
+        self.y_train = y_train
+        self.k = k
 
     @staticmethod
-    def test():
-        print("Método teste")
+    def euclidean_distance(a, b):
+        return np.linalg.norm(a - b)
+
+    def predict(self, test_sample):
+        distances = np.zeros((self.x_train.shape[0],))
+        for index, x_i in enumerate(self.x_train):
+            distances[index] = self.euclidean_distance(x_i, test_sample)
+        neighbours_x = distances.argsort()[:self.k]
+        predicted_y = stats.mode(self.y_train[neighbours_x]).mode[0]
+        return predicted_y
+
+
+def execution(self):
+    """
+    1. Load the data
+    2. Initialise the value of k
+    3. For getting the predicted class, iterate from 1 to total number of training data points
+        3.1. Calculate the distance between test data and each row of training data. Here we will use Euclidean
+        distance as our distance metric since it’s the most popular method. The other metrics that can be used are
+        Chebyshev, cosine, etc.
+        3.2. Sort the calculated distances in ascending order based on distance values
+        3.3. Get top k rows from the sorted array
+        3.4. Get the most frequent class of these rows
+        3.5. Return the predicted class
+    """
+    pass
--- main.py
+++ main.py
@@ -1,57 +1,54 @@
-import time
+"""
+1. Load the data
+2. Initialise the value of k
+3. For getting the predicted class, iterate from 1 to total number of training data points
+    3.1. Calculate the distance between test data and each row of training data. Here we will use Euclidean
+    distance as our distance metric since it’s the most popular method. The other metrics that can be used are
+    Chebyshev, cosine, etc.
+    3.2. Sort the calculated distances in ascending order based on distance values
+    3.3. Get top k rows from the sorted array
+    3.4. Get the most frequent class of these rows
+    3.5. Return the predicted class
+"""
+from sklearn.datasets import load_digits
+import numpy as np
+from knn.core import KNearestNeighborsCore
 
-import torch
-from matplotlib import pyplot as plt
+# Load dataset
+digits = load_digits(n_class=10, return_X_y=False)
+X = digits.data
+Y = digits.target
 
-from pykeops.torch import LazyTensor
+# Split dataset between train and test data
+n_train, n_test = (1000, 100)
+x_train, y_train = X[:n_train, :], Y[:n_train]
+x_test, y_test = X[n_train:n_train + n_test, :], Y[n_train:n_train + n_test]
 
-use_cuda = torch.cuda.is_available()
-tensor = torch.cuda.FloatTensor if use_cuda else torch.FloatTensor
+# Instance of knn model
+knn_instance = KNearestNeighborsCore(x_train, y_train, k=5)
 
-# Load dataset MNIST
-try:
-    from sklearn.datasets import fetch_openml
-except ImportError:
-    raise ImportError("This tutorial requires Scikit Learn version >= 0.20.")
+# Params
+times = 100
+acc = 0
 
-mnist = fetch_openml('mnist_784', cache=False)
+for i in range(times):
+    print("\n###########################")
+    print("Round: ", i + 1)
+    # Choose new test sample
+    i_test = np.random.choice(np.arange(x_test.shape[0]))
+    xi_test = x_test[i_test]
+    yi_test = y_test[i_test]
+    yi_predicted = knn_instance.predict(xi_test)
+    hit = yi_test == yi_predicted
 
-x = tensor(mnist.data.astype('float32'))
-y = tensor(mnist.target.astype('int64'))
+    # Print output
+    # print("xi (test):\n", xi_test)
+    print("y:", yi_test, "\ny(predicted):", yi_predicted, "\n(HIT)" if hit else "\n(MISS)")
 
-# Split dataset
-D = x.shape[1]
-Ntrain, Ntest = (60000, 10000) if use_cuda else (1000, 100)
-x_train, y_train = x[:Ntrain, :], y[:Ntrain]
-x_test, y_test = x[Ntrain:Ntrain + Ntest, :], y[Ntrain:Ntrain + Ntest]
+    # Accuracy
+    if hit:
+        acc += 1
+    print("###########################")
 
-# KNN search
-K = 3  # N.B.: K has very little impact on the running time
-
-start = time.time()  # Benchmark:
-
-X_i = LazyTensor(x_test[:, None, :])  # (10000, 1, 784) test set
-X_j = LazyTensor(x_train[None, :, :])  # (1, 60000, 784) train set
-D_ij = ((X_i - X_j) ** 2).sum(-1)  # (10000, 60000) symbolic matrix of squared L2 distances
-
-ind_knn = D_ij.argKmin(K, dim=1)  # Samples <-> Dataset, (N_test, K)
-lab_knn = y_train[ind_knn]  # (N_test, K) array of integers in [0,9]
-y_knn, _ = lab_knn.mode()  # Compute the most likely label
-
-if use_cuda: torch.cuda.synchronize()
-end = time.time()
-
-error = (y_knn != y_test).float().mean().item()
-time = end - start
-
-print("{}-NN on the full MNIST dataset: test error = {:.2f}% in {:.2f}s.".format(K, error * 100, time))
-
-# Fancy plot
-plt.figure(figsize=(12, 8))
-for i in range(6):
-    ax = plt.subplot(2, 3, i + 1)
-    ax.imshow((255 - x_test[i]).view(28, 28).detach().cpu().numpy(), cmap="gray")
-    ax.set_title("label = {}".format(y_knn[i].int()))
-    plt.axis('off')
-
-plt.show()
+print("\nRESULTS: ")
+print("Accuracy = ", (acc / times) * 100, "%")
